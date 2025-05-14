c06c49e6d5829a6a3febf4538bbf2bf445138f90
fasasimayokun <maykay.fash@gmail.com>
2025-05-14 09:22:15 +0100

    2- updateAnimalPost controller added

--- backend/controllers/animal.controller.js
+++ backend/controllers/animal.controller.js
@@ -79,3 +79,59 @@ export const getAllAnimalPost = async (req, res) => {
         res.status(500).json({ error: "Internal server error"});
     }
 };
+
+export const getSingleAnimalPost = async (req, res) => {
+    try {
+        const animalId = req.params.id;
+        const animal = await Animal.findById(animalId).populate({
+            path: "comments.author",
+            select: "username email profileImg"
+        }).populate({
+            path: "comments.replies.author",
+            select: "username email profileImg"
+        });
+
+        if(!animal) {
+            return res.status(404).json({ error: "Animal not found"});
+        }
+
+        res.status(200).json(animal);
+    } catch (error) {
+        console.log("Error in getSingleAnimalPost", error.message);
+        res.status(500).json({ error: "Internal server error"});
+    }
+};
+
+export const updateAnimalPost = async (req, res) => {
+    try {
+        const { name, description, habitat, facts} = req.body;
+        let { image } =  req.body;
+
+        let animal = await Animal.findById(req.params.id);
+
+        if(!animal) {
+            return res.status(404).json({ error: "Animal not found"});
+        }
+
+        if(image) {
+            if (animal.image) {
+                await cloudinary.uploader.destroy(animal.image.split("/").pop().split(".")[0]);
+            }
+            const uploadedResponse = await cloudinary.uploader.upload(image);
+            image = uploadedResponse.secure_url;
+        }
+
+        animal.name = name || animal.name;
+        animal.image = image || animal.image;
+        animal.description = description || animal.description;
+        animal.habitat =  habitat || animal.habitat;
+        animal.facts = facts || animal.facts;
+
+        animal = await animal.save();
+
+        res.status(200).json(animal);
+    } catch (error) {
+        console.log("Error in the updateAnimalPost", error.message);
+        res.status(500).json({ error: "Internal server error"});
+    }
+};
--- backend/routes/animal.route.js
+++ backend/routes/animal.route.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { createAnimalPost, deleteAniamlPost, getAllAnimalPost } from "../controllers/animal.controller.js";
 import { protectRoute } from "../middleware/protectRoute.js";
+import { createAnimalPost, deleteAniamlPost, getAllAnimalPost,
+    getSingleAnimalPost, updateAnimalPost } from "../controllers/animal.controller.js";
 
 const router = express.Router();
 
 router.get("/all", protectRoute, getAllAnimalPost);
+router.get("/:id", protectRoute, getSingleAnimalPost);
 router.post("/create", protectRoute, createAnimalPost);
+router.post("/update/:id", protectRoute, updateAnimalPost);
 router.delete("/:id", protectRoute, deleteAniamlPost);
 
 export default router;
